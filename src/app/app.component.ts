9a1a048182ebc12393f5df0a0d2c0e00a0626201
DavideBelvedere <davide.belvedere@gmail.com>
2018-04-09 18:07:14 +0200

    sistemato piccolo errore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,9 +24,9 @@ export class AppComponent {
       this.modal = modal;
       this.showModal = modal != null && modal != undefined;
 
-      this.loginService.logged$.subscribe((login: boolean) => {
-        this.isLogged = login;
-      })
+    });
+    this.loginService.logged$.subscribe((login: boolean) => {
+      this.isLogged = login;
     })
   }
 
